27e89bad77b88d58c91a16a8d2b7e1cd87e583ca
Martin <redalpha888@gmail.com>
2015-08-01 19:44:06 -0500

    Despliegue en Heroku agregar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -29,3 +29,17 @@ exports.index=function(req,res){
 			res.render('quizes/index.ejs',{quizes:quizes});
 	}).catch(function(error){next(error);});
 };
+//get /quizes/new
+exports.new=function(req,res){
+	var quiz=models.Quiz.build(//crea objeto quiz
+		{pregunta:"Pregunta",respuesta:"Respuesta"});
+	res.render('quizes/new',{quiz:quiz});
+};
+//post /quizes/create
+exports.create=function(req,res){
+	var quiz=models.Quiz.build(req.body.quiz);
+	//guarda en Db los campos pregunta y respuesta de quiz
+	quiz.save({fields:["pregunta","respuesta"]}).then(function(){
+		res.redirect('/quizes');  //redireccion a lista de preguntas
+	})
+};
